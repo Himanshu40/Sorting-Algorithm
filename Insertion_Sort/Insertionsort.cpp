8660890be370294228a6c6d6c1f79a7901537358
Himanshu Sekhar Nayak <himanshuwindows8.1@gmail.com>
2020-10-06 19:55:47 +0530

    Added Indentation where necessary v1.0

--- Insertion_Sort/Insertionsort.cpp
+++ Insertion_Sort/Insertionsort.cpp
@@ -1,37 +1,49 @@
-#include<bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
 void display(int *array, int size) {
-   for(int i = 0; i<size; i++)
+   for(int i = 0; i < size; i++)
       cout << array[i] << " ";
    cout << endl;
 }
 
-void insertionSort(int *array,int size) {
+void insertionSort(int *array, int size) {
    int key, j;
-   for(int i = 1; i<size; i++) {
-      key = array[i]; //take value
+
+   for(int i = 1; i < size; i++) {
+      key = array[i];   //take value
       j = i;
+
       while(j > 0 && array[j-1]>key) {
-         array[j] = array[j-1];
-         j--;
-        }
-     array[j] = key;   //insert in right place
+        array[j] = array[j-1];
+        j--;
+      }
+
+      array[j] = key;   //insert in right place
    }
 }
 
 int main() {
    int n;
-   cout<<"Enter the number of elements: ";
-   cin>>n;
-   int arr[n];//create an array with given number of elements
-   cout<<"Enter elements:"<<endl;
-   for(int i = 0; i<n; i++) {
+
+   cout << "\n\t---INSERTION SORT---\n";
+   cout << "Enter the number of elements: ";
+   cin >> n;
+
+   int arr[n];          //create an array with given number of elements
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i < n; i++) {
       cin >> arr[i];
    }
-   cout<<"Array before Sorting: ";
+
+   cout << "Array before Sorting: ";
    display(arr,n);
+   
    insertionSort(arr,n);
+   
    cout<<"Array after Sorting: ";
    display(arr, n);
+
+   return 0;
 }
