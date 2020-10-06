9a292ce36fa052d876e618402cb75977e93503ee
Himanshu Sekhar Nayak <himanshuwindows8.1@gmail.com>
2020-10-06 19:45:49 +0530

    Indented where necessary

--- Bubble_Sort/BubbleSort.cpp
+++ Bubble_Sort/BubbleSort.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 void swapping(int &a, int &b) { //swap the content of a and b
@@ -7,20 +8,24 @@ void swapping(int &a, int &b) { //swap the content of a and b
    a = b;
    b = temp;
 }
-void display(int *array,int size) {
-   for(int i = 0; i<size; i++)
+
+void display(int *array, int size) {
+   for(int i = 0; i < size; i++)
       cout << array[i] << " ";
    cout << endl;
 }
-void bubbleSort(int *array,int size){
-   for(int i=0; i<size; i++) {
-      int swaps = 0;    //flag to detect any swap is there or not
-      for(int j = 0; j<size-i-1; j++) {
+
+void bubbleSort(int *array, int size) {
+   for(int i=0; i < size; i++) {
+      bool swaps = false;    //flag to detect any swap is there or not
+      
+      for(int j = 0; j < size - i - 1; j++) {
          if(array[j] > array[j+1]) {    //when the current item is bigger than next
             swapping(array[j], array[j+1]);
-            swaps = 1;    //set swap flag
+            swaps = true;    //set swap flag
          }
       }
+      
       if(!swaps)
          break;       // No swap in this pass, so array is sorted
    }
@@ -28,16 +33,24 @@ void bubbleSort(int *array,int size){
 
 int main() {
    int n;
+
+   cout << "\n\t---BUBBLE SORT---\n";
    cout << "Enter the number of elements: ";
    cin >> n;
+
    int arr[n];     //create an array with given number of elements
    cout << "Enter elements:" << endl;
-   for(int i = 0; i<n; i++) {
+   for(int i = 0; i < n; i++) {
       cin >> arr[i];
    }
+
    cout << "Array before Sorting: ";
    display(arr, n);
+
    bubbleSort(arr, n);
+
    cout << "Array after Sorting: ";
    display(arr, n);
+
+   return 0;
 }
