353a0c2a6e6434a16c4f6c0de4f8206acd41a692
immortalJAI <60728208+Jaidev2001@users.noreply.github.com>
2020-10-10 12:55:26 +0530

    Update quicksort.cpp

--- Quick_Sort/quicksort.cpp
+++ Quick_Sort/quicksort.cpp
@@ -50,9 +50,16 @@ void printArray(int arr[], int size)
  
 int main()  
 {  
-    int arr[] = {10, 7, 8, 9, 1, 5};  
-    int n = sizeof(arr) / sizeof(arr[0]);  
-    quickSort(arr, 0, n - 1);  
+    int n;
+    cout<<"Enter the size of the array\n";
+    cin>>n;
+    int arr[n+1];
+    cout<<"Enter the elements of array\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    quickSort(arr,0,n); 
     cout << "Sorted array: \n";  
     printArray(arr, n);  
     return 0;  
