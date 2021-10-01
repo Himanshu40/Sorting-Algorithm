c5b53b1498cb762e7f0a4ebd0f67ac51d4c32783
addy0110 <addycoder0110@gmail.com>
2021-10-01 13:52:08 +0530

    Create Counting_Sort.cpp
    Added counting sort code in C++

---
+++ Counting_Sort/Counting_Sort.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int smallInt = -1e9;
+int largeInt = 1e9;
+// Counting Sort
+// Note: Range of the input array can't exceed max size that can be assigned to a vector in your system.
+
+int findMax(vector<int>&vec){
+    int currMax = smallInt;
+    for(int i = 0 ; i < vec.size() ; i++){
+        if(currMax < vec[i]){
+            currMax = vec[i];
+        }
+    }
+    return currMax;
+}
+
+int findMin(vector<int>&vec){
+    int currMin = largeInt;
+    for(int i = 0 ; i < vec.size() ; i++){
+        if(currMin > vec[i]){
+            currMin = vec[i];
+        }
+    }
+    return currMin;
+}
+
+void countSort(vector<int>&vec){
+    int maxElement = findMax(vec);
+    int minElement = findMin(vec);
+    int range = maxElement - minElement + 1;
+ 
+    vector<int>count(range,0);
+    vector<int>output(vec.size());
+    for(int i = 0; i < vec.size(); i++)
+        count[vec[i] - minElement]++;
+ 
+    for(int i = 1; i < count.size(); i++)
+        count[i] += count[i - 1];
+ 
+    for(int i = vec.size() - 1; i >= 0; i--) {
+        output[count[vec[i] - minElement] - 1] = vec[i];
+        count[vec[i] - minElement]--;
+    }
+ 
+    for(int i = 0; i < vec.size(); i++)
+    vec[i] = output[i];
+}
+int main(){
+    int n;
+    cout<<"Enter number of elements : ";
+    cin>>n;
+    cout<<"Enter elements : ";
+    vector<int>vec(n);
+    for(int i = 0 ; i < n ; i++){
+        cin>>vec[i];
+    }
+    countSort(vec);
+    cout<<"Sorted : ";
+    for(int i = 0 ; i < n ; i++){
+        cout<<vec[i]<<" ";
+    }
+    return 0;
+}
