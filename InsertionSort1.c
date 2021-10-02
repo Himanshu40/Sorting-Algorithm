1ca8fa0a6d9fb69ba2078536ace0675e63972002
Himanshu Sekhar Nayak <himanshuwindows8.1@gmail.com>
2021-10-02 10:21:35 +0530

    Rename InsertionSort.c to InsertionSort1.c to avoid merge conflicts

--- InsertionSort1.c
+++ InsertionSort1.c
@@ -3,7 +3,7 @@
 // Insertion Sort Function
 void insertionSort(int array[], int n) 
 { 
-int i, element, j; 
+ int i, element, j; 
 for (i = 1; i < n; i++) { element = array[i]; j = i - 1; while (j >= 0 && array[j] > element) { 
 array[j + 1] = array[j]; 
 j = j - 1; 
